ed47a209a9a0e3f107cc1be68778b7f85bd50ad2
Alex Lv <lvxineye@gmail.com>
2015-11-26 17:25:27 +0800

    add BaseApp

---
+++ appLibrary/src/main/java/com/android/lvxin/sdk/service/AppService.java
@@ -0,0 +1,35 @@
+package com.android.lvxin.sdk.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+/**
+ * @ClassName: AppService
+ * @Author: lvxin
+ * @Description: 随App启动的全局Service
+ * @Date: 11/26/15 16:10
+ */
+public class AppService extends Service {
+
+    private ServiceBinder mBinder;
+
+    public AppService() {
+        mBinder = new ServiceBinder();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    static class ServiceBinder extends Binder {
+
+    }
+}
